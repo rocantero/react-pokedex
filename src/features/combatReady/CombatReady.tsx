11b5b4098b23788be56d71b59ebe57f06cbd02d1
Rodrigo Cantero <rodrigo@lyradesigns.com>
2024-11-08 12:12:48 -0300

    fix bug

--- src/features/combatReady/CombatReady.tsx
+++ src/features/combatReady/CombatReady.tsx
@@ -7,8 +7,7 @@ import {
 import { Pokemon } from "../pokemon/Pokemon"
 
 export const CombatReady = () => {
-  const dispatch = useAppDispatch()
-  const combatReady = useAppSelector((state: { combatReady: Pokemon[] }) => state.combatReady)
+  const combatReady = useAppSelector((state) => state.combatReady)
   const [hasPokemons, setHasPokemons] = useState(false)
 
   useEffect(() => {
@@ -17,8 +16,8 @@ export const CombatReady = () => {
 
   return (
     
-    <div className={styles.row}>
-      <h2 className={styles.title}>Listos para el combate</h2>
+    <div className={styles.row}>``
+      <h2 className={styles.title}>Listos para el combate</h2>1``
       <div className={styles.pokemons}>
       {
       hasPokemons ?
